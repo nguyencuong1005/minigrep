cd955b24ce05283b3be03777f395372be50050a3
Cuong Nguyen <nguyen.cuonguuu@gmail.com>
2025-03-03 23:00:37 +0700

    error handling and code graining

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Error {err}");
+        process::exit(1)
+    });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    run(config);
+}
 
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
     println!("With text:\n{contents}");
+    Ok(())
 }
 
 struct Config {
@@ -20,8 +28,17 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-    Config { query, file_path }
+impl Config {
+    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        Ok((Config {
+            // `args` is a reference to a `Vec<String>`, and since `query` and `file_path` are of type `String` (not `&str`),
+            // we need to clone the strings to take ownership without moving them out of the borrowed vector.
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        }))
+    }
 }
